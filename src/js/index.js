895497a2510b0715f30b19cb120ad15d7a5323ec
Alexander Lyapko <sunsay@maprox.net>
2013-01-10 13:20:40 +0400

    #1790

--- src/js/index.js
+++ src/js/index.js
@@ -3,37 +3,29 @@
  * 2013, Maprox LLC
  */
 var app = {
-
-	storage: {
-		get: function(id) {
-			if (typeof(window.localStorage) != "undefined") {
-				return window.localStorage[id];
-			}
-		},
-		set: function(id, value) {
-			if (typeof(window.localStorage) != "undefined") {
-				window.localStorage[id] = value;
-			}
-		},
-		unset: function(id) {
-			if (typeof(window.localStorage) != "undefined") {
-				window.localStorage.removeItem(id);
-			}
-		}
-	},
-
 /**
-	* Application Constructor
+	* Constructor
 	*/
 	initialize: function() {
 		this.bindEvents();
 		this.bindHandlers();
+		this.initEnvironment();
 		// check authorization
 		if (!this.isAuthorized()) {
 			$.mobile.changePage("#registration");
 		} else {
 			$.mobile.changePage("#settings");
 		}
+	},
+
+/**
+	* Environment initialization
+	*/
+	initEnvironment: function() {
+		$.mobile.loader.prototype.options.text = "Loading...";
+		$.mobile.loader.prototype.options.textVisible = true;
+		$.mobile.loader.prototype.options.theme = "a";
+		$.mobile.loader.prototype.options.html = "";
 	},
 
 /**
@@ -51,10 +43,54 @@ var app = {
 	},
 
 /**
+	* 
+	*/
+	lock: function() {
+		$("body").append('<div class="modalWindow"/>');
+		$.mobile.loading('show');
+	},
+
+/**
+	* 
+	*/
+	unlock: function() {
+		$.mobile.loading('hide');
+		$(".modalWindow").remove();
+	},
+
+/**
 	* Bind button handlers
 	*/
 	bindHandlers: function() {
-		
+		$('#signup').on('tap', $.proxy(this.signup, this));
+	},
+
+/**
+	*
+	*/
+	signup: function() {
+		if (this.isSigningUp) { return; }
+		this.isSigningUp = true;
+		this.lock();
+		$.ajax({
+			url: app.cfg.path + '/mon_device/create',
+			dataType: 'jsonp',
+			type: 'POST',
+			data: {
+				
+			},
+			success: function(json) {
+				// do stuff with json (in this case an array)
+				alert("Success");
+			},
+			error: function() {
+				alert("Error");
+			}
+		});
+		setTimeout(function() {
+			app.unlock();
+			app.isSigningUp = false;
+		}, 2000);
 	},
 
 /**
@@ -91,4 +127,32 @@ var app = {
  */
 app.manager = {
 	
+};
+
+/**
+ * Storage implementation
+ */
+app.storage = {
+	get: function(id) {
+		if (typeof(window.localStorage) != "undefined") {
+			return window.localStorage[id];
+		}
+	},
+	set: function(id, value) {
+		if (typeof(window.localStorage) != "undefined") {
+			window.localStorage[id] = value;
+		}
+	},
+	unset: function(id) {
+		if (typeof(window.localStorage) != "undefined") {
+			window.localStorage.removeItem(id);
+		}
+	}
+};
+
+/**
+ * Application configuration
+ */
+app.cfg = {
+	path: 'http://observer.localhost/'
 };
