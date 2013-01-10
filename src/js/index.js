3cd85af89fb1a38652da4d8bfa5dd4229d28ef3f
Alexander Lyapko <sunsay@maprox.net>
2013-01-10 18:03:45 +0400

    #1790

--- src/js/index.js
+++ src/js/index.js
@@ -10,12 +10,6 @@ var app = {
 		this.bindEvents();
 		this.bindHandlers();
 		this.initEnvironment();
-		// check authorization
-		if (!this.isAuthorized()) {
-			$.mobile.changePage("#registration");
-		} else {
-			$.mobile.changePage("#settings");
-		}
 	},
 
 /**
@@ -26,13 +20,25 @@ var app = {
 		$.mobile.loader.prototype.options.textVisible = true;
 		$.mobile.loader.prototype.options.theme = "a";
 		$.mobile.loader.prototype.options.html = "";
+		// ajax error handling
+		$.ajaxSetup({
+			error: function(xhr){
+				var msg = 'Request Status: ' + xhr.status +
+					' Status Text: ' + xhr.statusText +
+					' ' + xhr.responseText;
+				console.warn(mgs);
+			}
+		});
+		// check authorization
+		$.mobile.changePage(!this.isAuthorized() ?
+			"#registration" : "#settings");
 	},
 
 /**
 	* Returns true, if this device was already authorized
 	*/
 	isAuthorized: function() {
-		return (typeof this.getDeviceKey() != "undefined");
+		return !!this.getDeviceKey();
 	},
 
 /**
@@ -43,19 +49,33 @@ var app = {
 	},
 
 /**
-	* 
+	* Sets devicekey value
+	* @param {String} value Device_key value
 	*/
-	lock: function() {
-		$("body").append('<div class="modalWindow"/>');
-		$.mobile.loading('show');
+	setDeviceKey: function(value) {
+		app.storage.set('devicekey', value);
 	},
 
 /**
-	* 
+	* Locks desktop while make ajax request
+	* @return {Boolean} False if already locked
+	*/
+	lock: function() {
+		if (!app.isLocked) {
+			$("body").append('<div class="modalWindow"/>');
+			$.mobile.loading('show');
+			app.isLocked = true;
+		}
+		return app.isLocked;
+	},
+
+/**
+	* Unlocks desktop
 	*/
 	unlock: function() {
 		$.mobile.loading('hide');
 		$(".modalWindow").remove();
+		app.isLocked = false;
 	},
 
 /**
@@ -69,28 +89,108 @@ var app = {
 	*
 	*/
 	signup: function() {
-		if (this.isSigningUp) { return; }
-		this.isSigningUp = true;
-		this.lock();
+		if (!this.lock()) { return; }
+
+		// make ajax request
 		$.ajax({
 			url: app.cfg.path + '/mon_device/create',
 			dataType: 'jsonp',
 			type: 'POST',
-			data: {
-				
-			},
-			success: function(json) {
-				// do stuff with json (in this case an array)
-				alert("Success");
+			data: app.getSignupParams(),
+			success: function(answer) {
+				// unlock application
+				app.unlock();
+				// 
+				console.log('Got the answer: ', answer);
+				if (!answer) {
+					return app.setSignupError('Wrong response from server');
+				}
+				if (answer.success) {
+					app.setDeviceKey(answer.device_key);
+					$.mobile.changePage("#settings");
+				} else {
+					var errorMessage = 'Unknown server error';
+					if (answer.errors) {
+						var error = answer.errors[0];
+						if (error.params && error.params.length) {
+							errorMessage = error.params[0];
+						}
+					}
+					return app.setSignupError(errorMessage);
+				}
 			},
 			error: function() {
-				alert("Error");
+				// unlock application
+				app.unlock();
+				// 
+				app.setSignupError('Unknown server error');
 			}
 		});
-		setTimeout(function() {
-			app.unlock();
-			app.isSigningUp = false;
-		}, 2000);
+	},
+
+/**
+	* Shows signup error
+	* @param {String} message
+	*/
+	setSignupError: function(message) {
+		if (!message) {
+			// hide message
+			$('#registration .errorBlock').hide();
+			return;
+		}
+		$('#registration .errorBlock').show();
+		$('#registration .errorBlock').html(message);
+	},
+
+/**
+	* Returns a json object of params, wich would be send to the maprox server
+	* @return {Object}
+	*/
+	getSignupParams: function() {
+		// required params
+		var shareKey = $('#share_key').val();
+		var params = {
+			uid: app.getDeviceUid(),
+			"share_key": shareKey
+		};
+		// optional parameters
+		var name = $('#devicename').val();
+		if (name) {
+			params.name = name;
+		}
+		var note = app.getDeviceInfo();
+		if (note) {
+			params.note = note;
+		}
+		//return params;
+		console.log(params);
+		return {};
+	},
+
+/**
+	* Returns the device uid
+	* @return {String}
+	*/
+	getDeviceUid: function() {
+		if (typeof(device) == "undefined") {
+			return 'DeviceObjectIsNotFound';
+		}
+		return device.uuid;
+	},
+
+/**
+	* Returns available the device information
+	* @return {String}
+	*/
+	getDeviceInfo: function() {
+		if (typeof(device) == "undefined") {
+			return null;
+		}
+		return 'Device Name: ' + device.name + '<br />' + 
+			'Device PhoneGap: ' + device.phonegap + '<br />' + 
+			'Device Platform: ' + device.platform + '<br />' + 
+			'Device UUID: ' + device.uuid + '<br />' + 
+			'Device Version: '  + device.version  + '<br />';
 	},
 
 /**
@@ -111,6 +211,9 @@ var app = {
 	*/
 	onDeviceReady: function() {
 		app.receivedEvent('deviceready');
+		if (typeof(device) != "undefined") {
+			$('#devicename').val(device.name);
+		}
 	},
 
 /*
@@ -154,5 +257,5 @@ app.storage = {
  * Application configuration
  */
 app.cfg = {
-	path: 'http://observer.localhost/'
+	path: 'https://observer.maprox.net/'
 };
